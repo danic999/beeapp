2d5723b4d7b8cdb9d59fd1f5c6c8535426fcb69e
mariola97 <maglica.mario68@gmail.com>
2022-05-26 15:27:56 +0200

    karta

--- app/screens/ShopMap.js
+++ app/screens/ShopMap.js
@@ -1,12 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React from "react";
-
 export default function ShopMap() {
   return (
-    <View>
-      <Text>ShopMap</Text>
-    </View>
+    <View style={styles.container}>
+     <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 43.821715,
+         longitude: 16.996858,
+         latitudeDelta: 0.005,
+         longitudeDelta: 0.0021,
+       }}
+     >
+     </MapView>
+   </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
